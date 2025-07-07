b18897c68463e352072f165b647696070e6c6824
Havva Ipek Avci <havvabuyukyalcin@gmail.com>
2025-07-07 20:26:50 +0200

    t6

--- src/MyClass.java
+++ src/MyClass.java
@@ -31,4 +31,7 @@ public class MyClass {
     //techpro branch version t5
     public String string10 = "Affet beni aksam ustu";
 
+    //conflict example,t6
+    public String string11 = "Bu aksam olurum beni kimse tutamaz";
+
 }
