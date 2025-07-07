b3032d5652cc9bdc8ebc4f0af7908c6dcd8306e0
Havva Ipek Avci <havvabuyukyalcin@gmail.com>
2025-07-07 21:27:34 +0200

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,4 +1,8 @@
 public class AnotherClass {
     //Techpro branch, version t7
     public String string1 = "Biri sen, biri ben, iki damla yas akti gozlerimden";
+
+    //techpro branch, t8
+
+    public String string2 = "Parildayip duran insanlara bak";
 }
